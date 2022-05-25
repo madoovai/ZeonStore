f0d25096ad85e511ea018753046c9075fce22013
aidana <aidana.borisova@gmail.com>
2022-05-25 11:12:21 +0600

    trello 175: - отделила модель изображения - подключила ckeditor - добавила незакомиченные файлы

--- store/settings.py
+++ store/settings.py
@@ -119,13 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, './static/images')
-STATIC_ROOT = 'store/static'
-MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = 'static'
+MEDIA_URL = '/media/'
 
-CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor"
+CKEDITOR_UPLOAD_PATH = "uploads/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
