7cc77bbebff77df4cc4d171d888fe6563e6e6797
aidana <aidana.borisova@gmail.com>
2022-05-27 15:40:08 +0600

    тикет Новости

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Product, Collection, ProductColor, ProductImage, AboutImage, About, OurAdvantage
+from store.models import Product, Collection, ProductColor, ProductImage, AboutImage, About, OurAdvantage, News
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -30,6 +30,10 @@ class OurAdvantageAdmin(admin.ModelAdmin):
     list_display = ['headline', 'description', 'icon']
 
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['headline', 'description', 'photo']
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Collection, CollectionAdmin)
 admin.site.register(ProductColor, ColorAdmin)
@@ -37,3 +41,4 @@ admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(AboutImage, AboutImageAdmin)
 admin.site.register(About, AboutAdmin)
 admin.site.register(OurAdvantage, OurAdvantageAdmin)
+admin.site.register(News, NewsAdmin)
