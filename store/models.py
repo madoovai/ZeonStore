167e2a66d98c53bacc55abc93b8fb724b80c5e6c
aidana <aidana.borisova@gmail.com>
2022-05-28 14:57:22 +0600

    тикет Товар - отобразила фотографии и цвета в Api для товара и похожих товаров

--- store/models.py
+++ store/models.py
@@ -34,6 +34,10 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def similar_products(self):
+        similar_products = Product.objects.filter(collection=self.collection).exclude(id=self.id)
+        return similar_products
+
     class Meta:
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
--- store/serializers.py
+++ store/serializers.py
@@ -1,18 +1,41 @@
 from rest_framework import serializers
-from store.models import Product, Collection, About, News, PublicOffer
+from store.models import Product, Collection, About, News, PublicOffer, ProductImage, Color
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ColorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Product
-        fields = ('title', 'articul', 'discount_price', 'old_price', 'fabric_structure',
-                  'fabric', 'discount', 'size_line', 'product_amount', 'collection', 'hit', 'latest', 'favorite')
+        model = Color
+        fields = ('name', 'rgb')
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    color = ColorSerializer()
+
+    class Meta:
+        model = ProductImage
+        fields = ('image', 'color')
 
 
 class SimilarProductSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True)
+
     class Meta:
         model = Product
-        fields = ('id', 'title', 'discount_price', 'old_price', 'discount', 'size_line', 'collection', 'favorite')
+        fields = ('id', 'title', 'discount_price', 'old_price', 'discount', 'size_line', 'collection',
+                  'favorite', 'images')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True)
+    similar_products = SimilarProductSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = (
+            'id', 'title', 'articul', 'discount_price', 'old_price', 'fabric_structure',
+            'fabric', 'discount', 'size_line', 'product_amount', 'collection', 'hit', 'latest', 'favorite',
+            'images', 'similar_products'
+        )
 
 
 class CollectionSerializer(serializers.ModelSerializer):
